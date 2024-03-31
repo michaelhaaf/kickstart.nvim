7a332e6a6a0eda90fc0a956aec5380e58b1b1286
michaelhaaf <michael.haaf@gmail.com>
2024-03-30 21:18:09 -0400

    add gitsigns and fugitive

--- init.lua
+++ init.lua
@@ -239,6 +239,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-fugitive', -- git integration
+  'tpope/vim-rhubarb', -- GitHub integration
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
